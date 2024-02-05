5db8fd5ada579fef350b5dc84a22f1ab4d6e9e09
Pablo Pomodoro <p.pomodoro@posteo.com>
2024-02-05 19:14:36 +0100

    PoC: remove sample app

--- Frontend/gmd-frontend/src/app/app.component.ts
+++ Frontend/gmd-frontend/src/app/app.component.ts
@@ -1,7 +1,5 @@
-import {Component, inject} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {HttpClient} from "@angular/common/http";
-import {CatFact} from "./CatFact";
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,30 +9,4 @@ import {CatFact} from "./CatFact";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  catFacts: CatFact[];
-  httpClient = inject(HttpClient);
-
-  public constructor() {
-    this.catFacts = [];
-    this.simulateServerResponseTime();
-  }
-
-  loadCatFacts() {
-    this.httpClient.get<CatFact[]>("https://cat-fact.herokuapp.com/facts").subscribe(result => {
-      this.catFacts = result;
-    });
-  }
-
-  simulateServerResponseTime() {
-    // simulate server response time
-    setTimeout(() => {
-      this.loadCatFacts()
-    }, 750);
-  }
-
-  shiftToNextFact() {
-    if (this.catFacts.length >= 0) {
-      this.catFacts.pop();
-    }
-  }
 }
--- Frontend/gmd-frontend/src/app/app.config.ts
+++ Frontend/gmd-frontend/src/app/app.config.ts
@@ -1,12 +1,10 @@
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import {HttpClientModule} from "@angular/common/http";
+import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
-    importProvidersFrom(HttpClientModule)
+    provideRouter(routes)
   ]
 };
