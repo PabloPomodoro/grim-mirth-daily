018ac6dc4decaa388d50c58cbc7b8f2f60b663d8
Pablo Pomodoro <p.pomodoro@posteo.com>
2024-02-15 19:14:28 +0100

    PoC: add post functionality mock + rest collection

--- gmd-backend/src/server.ts
+++ gmd-backend/src/server.ts
@@ -1,28 +1,32 @@
 import { Hono } from 'https://deno.land/x/hono@v3.4.1/mod.ts';
 
 const app = new Hono();
+let books = [
+    'Hermann Hesse - Siddhartha',
+    'Carolin Emcke - Gegen den Hass',
+    'A. J. Jacobs - Saufit',
+    'Terry Pratchett - Nur du hast den Schlüssel'
+];
+
 
 // Redirect root URL
 app.get("/", (c) => c.redirect("/books"));
 
 // List all books
 app.get("/books", async (c) => {
-    const books = [
-        'Hermann Hesse - Siddhartha',
-        'Carolin Emcke - Gegen den Hass',
-        'A. J. Jacobs - Saufit',
-        'Terry Pratchett - Nur du hast den Schlüssel'
-    ];
     return c.json(books);
 });
 
-/*// Create a book (POST body is JSON)
+// Create a book (POST body is JSON)
 app.post("/books", async (c) => {
-    const body = await c.req.json();
-    return c.json(result);
+    const jsonResponse = await c.req.json();
+    const jsonResponseValue = jsonResponse.value;
+
+    books.push(jsonResponseValue);
+    return c.json(books);
 });
 
-// Get a book by title
+/*// Get a book by title
 app.get("/books/:title", async (c) => {
     const title = c.req.param("title");
     return c.json(result);
