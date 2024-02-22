a6e7af350502678bab763ca4036c656034e37cc5
Pablo Pomodoro <p.pomodoro@posteo.com>
2024-02-22 01:11:13 +0100

    V1: way better welcome page!

--- gmd-frontend/src/app/components/welcome/welcome.component.ts
+++ gmd-frontend/src/app/components/welcome/welcome.component.ts
@@ -1,7 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {ActivatedRoute, Router} from '@angular/router';
-import {faCircleInfo, faSun} from '@fortawesome/free-solid-svg-icons';
+import {faCircleInfo, faCalendarDays} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-welcome',
@@ -16,7 +16,7 @@ export class WelcomeComponent {
   private router = inject(Router);
   private route = inject(ActivatedRoute);
 
-  faSun = faSun;
+  faCalendarDays = faCalendarDays;
   faCircleInfo = faCircleInfo;
 
   navigateToQA() {
