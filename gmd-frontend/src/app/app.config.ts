f32bde7ba8c26ec291d8a3e7a36aabf98767c4eb
Pablo Pomodoro <p.pomodoro@posteo.com>
2024-02-25 23:06:05 +0100

    feat(V1): transloco ROCKS!!

--- gmd-frontend/src/app/app.config.ts
+++ gmd-frontend/src/app/app.config.ts
@@ -1,12 +1,23 @@
-import {ApplicationConfig} from '@angular/core';
+import {ApplicationConfig, isDevMode} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@ngneat/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(), provideHttpClient(), provideTransloco({
+        config: {
+          availableLangs: ['en', 'de'],
+          defaultLang: 'en',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      })
   ]
 };
