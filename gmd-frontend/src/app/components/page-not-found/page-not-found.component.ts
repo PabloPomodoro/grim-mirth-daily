ca3be467e9ae395f13efb84be1e877cf1b041888
Pablo Pomodoro <p.pomodoro@posteo.com>
2024-02-28 23:14:11 +0100

    fix(V1): add alt to image as guidelines commands + remove unused icon

--- gmd-frontend/src/app/components/page-not-found/page-not-found.component.ts
+++ gmd-frontend/src/app/components/page-not-found/page-not-found.component.ts
@@ -1,21 +1,16 @@
 import {Component, inject} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {faCircleQuestion} from '@fortawesome/free-solid-svg-icons';
-import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [
-    FaIconComponent
-  ],
+  imports: [],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
 export class PageNotFoundComponent {
   private router = inject(Router);
   private route = inject(ActivatedRoute);
-  protected readonly faCircleQuestion = faCircleQuestion;
 
   gotoHome() {
     this.router.navigate(['/welcome'], { relativeTo: this.route });
