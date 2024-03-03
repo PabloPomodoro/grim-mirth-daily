8838ace89e70acc6056f97d67da0377effea1cca
Pablo Pomodoro <p.pomodoro@posteo.com>
2024-03-03 02:17:32 +0100

    feature(V1): add second step of qa dialog - pls ignore the step system, will be reworked

--- gmd-frontend/src/app/components/question-answer/question-answer.component.ts
+++ gmd-frontend/src/app/components/question-answer/question-answer.component.ts
@@ -2,9 +2,16 @@ import {
   Component,
   Directive,
   OnInit,
-  ElementRef
+  ElementRef,
+  EventEmitter,
+  Input,
+  inject,
+  Output,
+  AfterViewInit,
 } from '@angular/core';
 import {TranslocoPipe} from '@ngneat/transloco';
+import {User} from '../../models/user.model';
+import {FormsModule} from '@angular/forms';
 
 @Directive({
   standalone: true,
@@ -18,19 +25,53 @@ export class AutoFocus implements OnInit {
   }
 }
 
+@Directive({
+  standalone: true,
+  selector: '[appStartNextStep]',
+})
+export class StartNextStep implements OnInit {
+  @Input('appStartNextStep') nextStepId!: string;
+  @Output() startNextStepEvent = new EventEmitter();
+
+  ngOnInit(): void {
+    this.startNextStepEvent.emit(Number(this.nextStepId));
+  }
+}
+
 @Component({
   selector: 'app-question-answer',
   standalone: true,
-  imports: [TranslocoPipe, AutoFocus],
+  imports: [TranslocoPipe, AutoFocus, FormsModule, StartNextStep],
   templateUrl: './question-answer.component.html',
   styleUrl: './question-answer.component.css',
 })
 export class QuestionAnswerComponent {
-  steps = new Array(2).fill(false);
+  steps = new Array(4).fill(false);
+  user: User;
 
   constructor() {
+    // empty initialization
+    this.user = new User('', new Date());
+
     setTimeout(() => {
       this.steps[0] = true;
     }, 1500);
+  }
+
+  startCountdownToNextStep(nextStepNumber: number) {
+    setTimeout(() => {
+      this.steps[nextStepNumber] = true;
+    }, 1000);
+  }
+
+  setStepId(id: number) {
+    this.steps[id] = true;
+    setTimeout(() => {
+      this.unsetStepId(id);
+      this.steps[3] = true;
+    }, 1500);
+  }
+  unsetStepId(id: number) {
+    this.steps[id] = false;
   }
 }
