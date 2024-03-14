8f0ec344dc3aa6903ae1037b753e4ca0adbc66d6
Pablo Pomodoro <p.pomodoro@posteo.com>
2024-03-14 19:26:25 +0100

    feat(V1): some middleware features

--- gmd-backend/src/server.ts
+++ gmd-backend/src/server.ts
@@ -1,28 +1,33 @@
 import {Hono} from 'https://deno.land/x/hono/mod.ts';
-import {basicAuth, cors} from 'https://deno.land/x/hono/middleware.ts';
+import {
+  basicAuth,
+  bearerAuth,
+  cors,
+} from 'https://deno.land/x/hono/middleware.ts';
 
 const app = new Hono();
 
 app.use(cors({origin: 'https://www.grim-mirth-daily.com'}));
 
-app.use(
-  '/*',
-  basicAuth({
-    username: 'pablo',
-    password: '1337',
-  })
-);
 
 let books = [
   'Hermann Hesse - Siddhartha',
   'Carolin Emcke - Gegen den Hass',
   'A. J. Jacobs - Saufit',
-  'A. J. Jacobs - Saufit',
   'Terry Pratchett - Nur du hast den Schlüssel',
 ];
 
+const token = 'hotstuff';
+
 // Redirect root URL
-app.get('/', (c) => c.json('This is the entry point for the Grim Mirth Daily backend.'));
+app.get(
+  '/',
+  basicAuth({
+    username: 'pablo',
+    password: '1337',
+  }),
+  (c) => c.json('This is the entry point for the Grim Mirth Daily backend.'),
+);
 
 // List all books
 app.get('/books', async (c) => {
@@ -30,24 +35,12 @@ app.get('/books', async (c) => {
 });
 
 // Create a book (POST body is JSON)
-app.post('/books', async (c) => {
+app.post('/books', bearerAuth({token}), async (c) => {
   const jsonResponse = await c.req.json();
   const jsonResponseValue = jsonResponse.value;
 
   books.push(jsonResponseValue);
   return c.json(books);
 });
-
-/*// Get a book by title
-app.get("/books/:title", async (c) => {
-    const title = c.req.param("title");
-    return c.json(result);
-});
-
-// Delete a book by title
-app.delete("/books/:title", async (c) => {
-    const title = c.req.param("title");
-    return c.text("");
-});*/
 
 Deno.serve(app.fetch);
