1676fba9191df2fef1afacfe18362349104b829d
Pablo Pomodoro <p.pomodoro@posteo.com>
2024-03-15 01:22:59 +0100

    feat(V1): first login draft

--- gmd-frontend/src/app/app.routes.ts
+++ gmd-frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {Routes} from '@angular/router';
 import {QuestionAnswerComponent} from './components/question-answer/question-answer.component';
 import {WelcomeComponent} from './components/welcome/welcome.component';
 import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+import {LoginComponent} from './components/login/login.component';
 
 export const routes: Routes = [
   {path: 'welcome', component: WelcomeComponent},
   {path: 'qa', component: QuestionAnswerComponent},
+  {path: 'login', component: LoginComponent},
   {path: '', redirectTo: '/welcome', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent},
 ];
