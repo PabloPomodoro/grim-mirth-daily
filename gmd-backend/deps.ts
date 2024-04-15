1cf46bdec888a51302906d197951220eb8adcac8
Pablo Pomodoro <p.pomodoro@posteo.com>
2024-04-16 00:50:52 +0200

    feat(V1): use logger for hono

--- gmd-backend/deps.ts
+++ gmd-backend/deps.ts
@@ -1,4 +1,4 @@
 export {Hono} from 'https://deno.land/x/hono/mod.ts';
 export {HTTPException} from 'https://deno.land/x/hono/http-exception.ts';
-export {bearerAuth, cors} from 'https://deno.land/x/hono/middleware.ts';
+export {bearerAuth, cors, logger} from 'https://deno.land/x/hono/middleware.ts';
 export {accessToken} from 'https://deno.land/x/access_token@1.0.0/src/access_token.ts';
--- gmd-backend/src/server.ts
+++ gmd-backend/src/server.ts
@@ -1,9 +1,10 @@
-import {Hono, cors} from '../deps.ts';
+import {Hono, cors, logger} from '../deps.ts';
 import {login} from './login-controller.ts';
 import {quote} from './quote-controller.ts';
 
 const app = new Hono();
 app.use(cors({origin: '*'}));
+app.use(logger())
 
 app.route('/login', login);
 app.route('/quote', quote);
