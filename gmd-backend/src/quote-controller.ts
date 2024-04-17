046deb3f122708f4fcbf3bfa8f8de645f056fd51
Pablo Pomodoro <p.pomodoro@posteo.com>
2024-04-17 02:54:09 +0200

    feat(V1): new quote controller endpoints + updated user model

--- gmd-backend/src/quote-controller.ts
+++ gmd-backend/src/quote-controller.ts
@@ -3,8 +3,34 @@ import {token} from './login-controller.ts';
 
 const quote = new Hono();
 
-quote.get('/', bearerAuth({token: token}), c => {
-  return c.json('Wer and\'ren eine Bratwurst brät, hat selbst ein Bratwurstbratgerät.');
+const quotes = [
+  {id: 1, text: 'Wer and\'ren eine Bratwurst brät, hat selbst ein Bratwurstbratgerät.'},
+];
+
+quote.get('/:id', bearerAuth({token: token}), c => {
+  const id = c.req.param('id');
+  return c.json(quotes.find(quote => quote.id === id));
+});
+
+quote.get('/all', bearerAuth({token: token}), c => {
+  return c.json(quotes);
+});
+
+quote.post('/create', bearerAuth({token: token}), async c => {
+  const newId = Math.max(...quotes.map(quote => quote.id)) + 1;
+  const newQuote = {
+    id: newId,
+    text: await c.req.json
+  };
+  quotes.push(newQuote);
+  return c.text('Quote ' + newQuote + ' created!');
+});
+
+quote.get('/:id', bearerAuth({token: token}), c => {
+  const id = c.req.param('id');
+  const index = quotes.indexOf(quote => quote.id === id);
+  quotes.splice(index, 1);
+  return c.text('Quote with id: ' + id + ' removed!');
 });
 
 export {quote};
