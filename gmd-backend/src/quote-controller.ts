3f89b5fad6544af3b469b43e4fb2f9819960cd91
Pablo Pomodoro <p.pomodoro@posteo.com>
2024-04-21 20:36:04 +0200

    fix(V1): number parsing + check if index is not found

--- gmd-backend/src/quote-controller.ts
+++ gmd-backend/src/quote-controller.ts
@@ -4,33 +4,39 @@ import {token} from './login-controller.ts';
 const quote = new Hono();
 
 const quotes = [
-  {id: 1, text: 'Wer and\'ren eine Bratwurst brät, hat selbst ein Bratwurstbratgerät.'},
+  {
+    id: 1,
+    text: "Wer and'ren eine Bratwurst brät, hat selbst ein Bratwurstbratgerät.",
+  },
 ];
 
-quote.get('/:id', bearerAuth({token: token}), c => {
-  const id = c.req.param('id');
-  return c.json(quotes.find(quote => quote.id === id));
+quote.get('/:id', bearerAuth({token: token}), (c) => {
+  const id = Number(c.req.param('id'));
+  return c.json(quotes.find((quote) => quote.id === id));
 });
 
-quote.get('/all', bearerAuth({token: token}), c => {
+quote.get('/all', bearerAuth({token: token}), (c) => {
   return c.json(quotes);
 });
 
-quote.post('/create', bearerAuth({token: token}), async c => {
-  const newId = Math.max(...quotes.map(quote => quote.id)) + 1;
+quote.post('/create', bearerAuth({token: token}), async (c) => {
   const newQuote = {
-    id: newId,
-    text: await c.req.json
+    id: Math.max(...quotes.map((quote) => quote.id)) + 1,
+    text: await c.req.json(),
   };
   quotes.push(newQuote);
   return c.text('Quote ' + newQuote + ' created!');
 });
 
-quote.delete('/:id', bearerAuth({token: token}), c => {
-  const id = c.req.param('id');
-  const index = quotes.indexOf(quote => quote.id === id);
-  quotes.splice(index, 1);
-  return c.text('Quote with id: ' + id + ' removed!');
+quote.delete('/:id', bearerAuth({token: token}), (c) => {
+  const id = Number(c.req.param('id'));
+  const index = quotes.findIndex((quote) => quote.id === id);
+  if (index !== -1) {
+    quotes.splice(index, 1);
+    return c.text('Quote with id: ' + id + ' removed!');
+  } else {
+    return c.notFound();
+  }
 });
 
 export {quote};
